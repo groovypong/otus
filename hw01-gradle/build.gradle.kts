49a542c8dff65b3ce7326e513f412d205f888c00
Aleksey Mischanchuk <91007076+groovypong@users.noreply.github.com>
2026-04-02 19:42:47 +0300

    Домашнее задание №1

---
+++ hw01-gradle/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    application
+    alias(libs.plugins.shadow)
+}
+
+dependencies {
+    implementation(libs.guava)
+}
+
+application {
+    mainClass = "ru.otus.App"
+}
+
+tasks.shadowJar {
+    manifest {
+        attributes("Main-Class" to "ru.otus.HelloOtus")
+    }
+}
